f017243436e755595747778114c5fa643f7bfe9c
rahulrwt05 <rahulmarch2002@gmail.com>
2025-02-14 00:35:07 +0530

    updated files

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import dotenv from "dotenv"
-import express from "express"
-import morgan from "morgan"
-import { dbConnection } from "./utils/index.js"
-import path from "path"
-import { fileURLToPath } from "url"
-import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js"
-import routes from "./routes/index.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dhttp://localhost:3000otenv";
+import express from "express";
+import morgan from "morgan";
+import { dbConnection } from "./utils/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
+import routes from "./routes/index.js";
 
 //resolving dirname for es module
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-console.log(__dirname)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
-dotenv.config()
+dotenv.config();
 
-dbConnection()
+dbConnection();
 
-const PORT = process.env.PORT || 8800
+const PORT = process.env.PORT || 8800;
 
-const app = express()
+const app = express();
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-)
+  cors({
+    origin: "https://prodigyapp.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Add this!
+    exposedHeaders: ["Authorization"], // Allow headers to be read
+  })
+);
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan("dev"))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", routes)
+app.use("/api", routes);
 //Use the clent app
-app.use(express.static(path.join(__dirname, "/client/dist")))
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 //Render frontend for any path that user goes to
 app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"))
-)
-app.use(routeNotFound)
-app.use(errorHandler)
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
+app.use(routeNotFound);
+app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
